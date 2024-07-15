cb2976bf0d52f3409bdc10ac584d0bf213af73ad
kareemrogers-coder <kareemrogers27@gmail.com>
2024-07-15 03:39:33 -0400

    code wars practice

---
+++ codewars_p1.py
@@ -0,0 +1,5 @@
+#the following code counts the number of letters with a statment
+
+string = ("Hello world im a animal, call me wolf") #enter statment here
+
+print (string.count("l")) #enter the searchable letter 
